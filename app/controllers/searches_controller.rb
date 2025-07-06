b02d544215b64b16999fd858980ae72b1a5d9c22
EC2 Default User <ec2-user@ip-172-31-2-236.ap-northeast-1.compute.internal>
2025-07-06 10:58:41 +0000

    Add serch

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,13 @@
+class SearchesController < ApplicationController
+  def search
+    @model = params[:model]
+    @content = params[:content]
+    @method = params[:method]
+
+    if @model == "User"
+      @records = User.search_for(@content, @method)
+    else
+      @records = Book.search_for(@content, @method)
+    end
+  end
+end
